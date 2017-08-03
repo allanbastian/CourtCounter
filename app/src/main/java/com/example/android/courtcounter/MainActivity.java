a22716672c95419bb5f4217cb76d1667e99dbd4c
tillu101 <you@example.com>
2017-08-03 21:30:51 +0530

    I did this!

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -8,7 +8,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private int scoreTeamA = 0;
-    private int scoreTeamB = 0;
+    public int scoreTeamB = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
